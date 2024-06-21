6738841b03acf05fb3241fd2f7c63efc642ac42e
Scripo <scripo@gmail.com>
2024-06-21 15:20:09 -0400

    Added GAS plugin

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Serge Novikov
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Serge Novikov
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
