2a7e727b79664c1d9f14945e6b8331f1f55fa6d5
Scripo <scripo@gmail.com>
2024-08-02 17:31:57 -0400

    Default Attribute Effect

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Serge Novikov
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
