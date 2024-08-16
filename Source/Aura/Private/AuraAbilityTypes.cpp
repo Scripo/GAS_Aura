ce341ebbafa12f2626163d748e44d440ea5c67dc
Scripo <scripo@gmail.com>
2024-08-16 10:25:19 -0400

    Fix for Custom Gameplay Effect Context

--- Source/Aura/Private/AuraAbilityTypes.cpp
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -1,4 +1,4 @@
-#include "AuraAbilityType.h"
+#include "AuraAbilityTypes.h"
 
 bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
 {
--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -4,6 +4,7 @@
 
 #include "AuraAbilityTypes.generated.h"
 
+
 USTRUCT(BlueprintType)
 struct FAuraGameplayEffectContext : public FGameplayEffectContext
 {
@@ -18,13 +19,13 @@ public:
 	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
 
 	/** Returns the actual struct used for serialization, subclasses must override this! */
-	virtual UScriptStruct* GetScriptStruct() const
+	virtual UScriptStruct* GetScriptStruct() const override
 	{
 		return FGameplayEffectContext::StaticStruct();
 	}
 
 	/** Custom serialization, subclasses must override this */
-	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
 
 protected:
 
