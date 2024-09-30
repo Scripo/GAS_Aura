c9f46163c12e8ee9f56d1a0032d3ecd104e1cea2
Scripo <scripo@gmail.com>
2024-09-30 15:56:13 -0400

    Ability level requirement

--- Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
@@ -5,7 +5,10 @@
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "GameplayTagContainer.h"
+
 #include "AbilityInfo.generated.h"
+
+class UGameplayAbility;
 
 USTRUCT(BlueprintType)
 struct FAuraAbilityInfo
@@ -29,6 +32,12 @@ struct FAuraAbilityInfo
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TObjectPtr<const UMaterialInterface> BackgroundMaterial = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 LevelRequirement = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayAbility> Ability;
 };
 
 /**
